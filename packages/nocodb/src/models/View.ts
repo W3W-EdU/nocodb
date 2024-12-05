200d632d934a687d635a526875f08b12dde95325
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-12-05 20:25:33 +0000

    fix: custom url is part of ee only

--- packages/nocodb/src/models/View.ts
+++ packages/nocodb/src/models/View.ts
@@ -137,14 +137,6 @@ export default class View implements ViewType {
       if (view) {
         view.meta = parseMetaProp(view);
 
-        if (view.fk_custom_url_id) {
-          const customUrl = await CustomUrl.get({
-            id: view.fk_custom_url_id,
-          });
-
-          view.custom_url_path = customUrl.custom_path || null;
-        }
-
         await NocoCache.set(`${CacheScope.VIEW}:${view.id}`, view);
       }
     }
@@ -187,14 +179,6 @@ export default class View implements ViewType {
       );
 
       if (view) {
-        if (view.fk_custom_url_id) {
-          const customUrl = await CustomUrl.get({
-            id: view.fk_custom_url_id,
-          });
-
-          view.custom_url_path = customUrl.custom_path || null;
-        }
-
         await NocoCache.set(
           `${CacheScope.VIEW}:${fk_model_id}:${view.id}`,
           view,
@@ -235,13 +219,6 @@ export default class View implements ViewType {
       );
       if (view) {
         view.meta = parseMetaProp(view);
-        if (view.fk_custom_url_id) {
-          const customUrl = await CustomUrl.get({
-            id: view.fk_custom_url_id,
-          });
-
-          view.custom_url_path = customUrl.custom_path || null;
-        }
 
         await NocoCache.set(`${CacheScope.VIEW}:${fk_model_id}:default`, view);
       }
@@ -273,14 +250,6 @@ export default class View implements ViewType {
       );
       for (const view of viewsList) {
         view.meta = parseMetaProp(view);
-
-        if (view.fk_custom_url_id) {
-          const customUrl = await CustomUrl.get({
-            id: view.fk_custom_url_id,
-          });
-
-          view.custom_url_path = customUrl.custom_path || null;
-        }
       }
       await NocoCache.setList(CacheScope.VIEW, [modelId], viewsList);
     }
