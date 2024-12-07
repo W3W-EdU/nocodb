269281e62dfef2a584ae7b7d41b4be36ea0a4e26
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-12-07 09:13:16 +0000

    fix(nc-gui): change expand icon from all the places

--- packages/nc-gui/components/cell/TextArea.vue
+++ packages/nc-gui/components/cell/TextArea.vue
@@ -537,7 +537,7 @@ watch([isVisible, inputRef], (value) => {
             class="!p-0 !w-5 !h-5 !min-w-[fit-content]"
             @click.stop="onExpand"
           >
-            <component :is="iconMap.expand" class="transform group-hover:(!text-grey-800) text-gray-700 text-xs" />
+            <component :is="iconMap.maximize" class="transform group-hover:(!text-grey-800) text-gray-700 w-3 h-3" />
           </NcButton>
         </NcTooltip>
       </div>
